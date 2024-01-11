bc22627d754f3d645d9c8f5404dfca723be040fc
Daniel Goldacker <daniel-goldacker@hotmail.com>
2024-01-11 20:48:25 -0300

    changed PRIVATE_KEY value

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 class ConfigFiles():
-    PRIVATE_KEY = 'chave_super_secreta'
+    PRIVATE_KEY = 'P17B8JZH1PRACVEC5W4IJXH2FZKD6ZFB8T5OAEFWG7VHJE40MQ'
     DATABASE_SQLITE = 'src/database/authenticator.db'
     BRAZIL_TIME_ZONE = 'America/Sao_Paulo'
     TIME_EXPIRATION_TOKEN_IN_MINUTES = 60
